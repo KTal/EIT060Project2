8e89a9c37d5a17330cd5bda364541be6d0f2ae45
Kasper <kurs07kta@student.lu.se>
2016-02-19 22:50:07 +0100

    Updated class MedicalRecord

---
+++ EIT060Project2/src/medicalRecords/MedicalRecord.java
@@ -0,0 +1,72 @@
+package medicalRecords;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MedicalRecord 
+{
+	int runningNbr;
+	Date date;
+	String department;
+	String doctorName;
+	String nurseName;
+	String note;
+	
+	public MedicalRecord(int runningNbr, Date date, String department, 
+			String doctorName, String nurseName, String note) 
+	{
+		this.runningNbr = runningNbr;
+		this.date = date;
+		this.department = department;
+		this.doctorName = doctorName;
+		this.nurseName = nurseName;
+		this.note = note;
+	}
+
+	public Date getDate() 
+	{
+		return date;
+	}
+
+	public String getDepartment() 
+	{
+		return department;
+	}
+
+	public String getDoctorName() 
+	{
+		return doctorName;
+	}
+
+	public String getNurseName() 
+	{
+		return nurseName;
+	}
+
+	public String getNote() 
+	{
+		return note;
+	}
+	
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append("RunNo: " );
+		sb.append(Integer.toString(runningNbr));
+		sb.append("\n");
+		
+		sb.append("Date: " );
+		Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String dateString = formatter.format(date);
+		sb.append(dateString);
+		sb.append("\n");
+		
+		sb.append("Department: " + department + "\n");
+		sb.append("Doctor: " + doctorName + "\n");
+		sb.append("Nurse: " + nurseName + "\n");
+		sb.append("Note: " + note  + "\n");
+		
+		return sb.toString();
+	}
+}
