28767824fbce1c290b73b27fe35902f85817d0cc
Kasper <kurs07kta@student.lu.se>
2016-02-19 22:28:56 +0100

    Updated class UserCredentials.

--- EIT060Project2/src/medicalRecords/UserCredentials.java
+++ EIT060Project2/src/medicalRecords/UserCredentials.java
@@ -2,5 +2,39 @@ package medicalRecords;
 
 public class UserCredentials 
 {
+	String userID;
+	UserCategory uc;
+	String name;
+	String socialSecNo;
+	String department;
 	
+	public UserCredentials(String userID, UserCategory uc, String name, 
+			String socialSecNo, String department)
+	{
+		this.userID = userID;
+		this.uc = uc;
+		this.name = name;
+		this.socialSecNo = socialSecNo;
+		this.department = department;
+	}
+	
+	public UserCategory getUc() {
+		return uc;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSocialSecNo() {
+		return socialSecNo;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
 }
